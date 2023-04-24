7b3e4eaceff9f3364e3f580a13589f9173885143
ZuoTiJia <2239651886@qq.com>
2023-04-24 20:21:12 +0800

    fix aggregate partial merge expr not match

--- datafusion/core/src/physical_plan/aggregates/row_hash.rs
+++ datafusion/core/src/physical_plan/aggregates/row_hash.rs
@@ -151,7 +151,9 @@ impl GroupedHashAggregateStream {
             let n_fields = match mode {
                 // In partial aggregation, we keep additional fields in order to successfully
                 // merge aggregation results downstream.
-                AggregateMode::Partial => expr.state_fields()?.len(),
+                AggregateMode::Partial | AggregateMode::PartialMerge => {
+                    expr.state_fields()?.len()
+                }
                 _ => 1,
             };
             // Stores range of each expression:
